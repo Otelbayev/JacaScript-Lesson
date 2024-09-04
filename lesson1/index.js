ef280d8b45b00a60eb2d4257eb721ca452091b82
jasurbrk <jasurdev1604@gmail.com>
2024-09-04 21:10:08 +0500

    Javascript lesson 1

---
+++ lesson1/index.js
@@ -0,0 +1,125 @@
+//! Variables -> let, const, var
+
+let name = "Eshamt.";
+
+const age = 20;
+
+var surname = "Toshmatov";
+
+// console.log(name);
+
+// console.log(age);
+
+// console.log(surname);
+
+console.log("+++++++++++++++++++++++++++++++++++++++");
+
+name = "Xolmat";
+surname = "Gulmatov";
+// age = 50;
+
+// console.log(name);
+
+// console.log(age);
+
+// console.log(surname);
+
+// const a = "none";
+
+// a = 50;  `` '' ""
+
+// ! DataTypes
+// String, Number, BigInt, Boolean, Null, Undefined, Symbol -> Primitive
+// Object -> Non-Primitive
+
+let str = "Hllo my name is Robert!";
+
+// console.log(str);
+
+// let num = 18;
+
+// console.log(num);
+
+// let big = 1n;
+
+// console.log(5n + 5n);
+
+const n = 9007199254740991n;
+
+// console.log(n + 2n);
+
+// 9007199254740993
+
+const confirm = false;
+
+// console.log(true);
+
+let s = null;
+let b;
+
+s = "dfkjbdfb`";
+
+// console.log(s);
+// console.log(b);
+
+let symbol1 = Symbol("s1zkvjndfkjvn");
+let symbol2 = Symbol("dfkmldflkdfmvklmdf");
+
+// console.log(symbol1);
+// console.log(symbol2);
+
+const num1 = 50;
+const num2 = 50;
+
+// console.log("str" === "strsddsv");
+
+// ! NON Primitive, Reference
+
+// Object , Array , function
+
+const obj = {
+  name: "Jasurbek",
+  age: "20",
+  job: "developer",
+};
+
+// console.log(obj.name, obj.age, obj.job);
+
+const arr = ["ai", 121, 5454n, "skjksdv"];
+
+// console.log(arr[0])
+
+function myName() {
+  console.log("Hello my name is John!");
+}
+
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+
+// typeof
+
+const number1 = 10;
+const str1 = "sdjnsvkdjsndvkjsndknskjv";
+const big = 5565465454n;
+const bool = true;
+const stym = Symbol("xvbxv");
+const p = null;
+const x = undefined;
+
+console.log(typeof x , p)
+
+
+console.log(null == undefined)
---
+++ lesson1/script.js
@@ -0,0 +1,3 @@
+const a = "none";
+
+a = 50;
