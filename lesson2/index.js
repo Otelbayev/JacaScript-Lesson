d3d415d1f3512167aa15040faa09ca11011aa450
jasurbrk <jasurdev1604@gmail.com>
2024-09-06 20:31:02 +0500

    Type Conversion and Type Coersion

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,1 +1,77 @@
-const a = 1;
+// String, Number, bigint, boolean, null , undefined, symbol
+// Object
+
+let mytStr = "John";
+("");
+let str3 = `Hello kvbjdhvb`;
+
+let str2 = `${str3} Aipply Academy`;
+
+// console.log(str2);
+
+//! Type convertion
+
+// ? String to Number
+
+let matn = "123";
+
+// console.log(matn); // 123 str
+
+matn = Number(matn);
+
+// console.log(typeof matn); // num
+// console.log(matn); // 123 num
+
+// console.log(Number("dkfjvndkjfvndkjf")); // Not a number NaN
+
+// ? Number to String
+let yosh = 25;
+let yoshMatn = String(yosh);
+// console.log(yoshMatn); // 25 str
+// console.log(typeof yoshMatn); // str
+
+// ? to Boolean
+
+let qiymat = "jnsjdn";
+let natija = Boolean(qiymat);
+// console.log(natija);
+
+//! Type Coercion
+
+// console.log("10" + 55); //string 105
+// console.log(55 + "10");
+// console.log(55 + +"10");
+
+// true 1
+// false 0
+
+// console.log(50 * false)
+
+// console.log(true + true*false)
+
+// console.log(+"dlkjvndfnv")
+
+// console.log("100" - 50);
+
+// console.log(50 - "100");
+
+// console.log("str1" + "str2");
+
+// console.log(50-500)
+
+// console.log(100*5)
+
+// console.log(5 % 2);
+
+// console.log(5 ** 5);
+
+//! =
+
+let a = 10;
+
+// console.log(5 == "5");
+// console.log(5 === "5");
+
+// console.log(`hello ${50}`)
+
+console.log("hello\tmy name is \n Robot!");
