5072bbdef2801a1c99418ed607698112267ba6a1
jasurbek <jasurdev1604@gmail.com>
2024-09-09 18:44:12 +0500

    Lessin 2

---
+++ lesson2/script.js
@@ -0,0 +1,54 @@
+const str = "123";
+const str1 = "Lorem";
+
+// console.log(typeof str);
+// console.log(typeof Number(str));
+
+const num1 = 555555555;
+
+// console.log(num1);
+// console.log(typeof String(num1));
+
+let str2 = "Lorem";
+
+str2 = Number(str2);
+
+// console.log(str2);
+
+const a = -99999999999000;
+
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(false));
+
+// console.log(true + false + (true + true) * 10);
+
+// 0 "" undefined null NaN false
+
+// console.log("5" - 10);
+// console.log(10 - "5");
+
+// true 1
+// false 0
+
+// console.log(true + 50)
+
+// ! operators
+
+console.log(5 % 2);
+
+// n % 2 === 1  toq
+// n % 2 === 0  jjuft
+
+// console.log(5 ** 2); 25
+
+""
+''
+const name = "Johns"
+const age = 21
+
+console.log(`My name is ${name} and I am ${age} \n years\told`)
+
+console.log(" -9 " - 5);
+console.log(" -9 " + 5) 
