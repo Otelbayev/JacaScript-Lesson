e3586d34dbf2f664415f93d48372f806e00123da
jasurbrk <jasurdev1604@gmail.com>
2024-09-09 20:21:39 +0500

    lesson 2 operators and If-else, Ternary

---
+++ lesson3/index.js
@@ -0,0 +1,133 @@
+// if (candition) {
+//     code;
+// } else {
+//     code;
+// }
+
+// if (true) {
+//   console.log("kjvvdv");
+// } else {
+//   console.log("False");
+// }
+
+// if ("bvjbjhb") {
+//   console.log(10);
+// } else {
+//   console.log(0);
+// }
+
+// 0-16, 16-35, 35+
+
+const age = 55;
+
+// if (age >= 0 && age <= 16) {
+//     console.log("Ruxsat etilmaydi!")
+// }  
+// if ( age > 16 && age <= 35) {
+//     console.log("Ruxsat etilgan")
+// }
+// if (age > 50 && age < 60) {
+//     console.log("Sog'liq uchun zarar!")  
+// }
+
+// if (age > 50 && age < 60) {
+//     console.log("Sog'liq uchun zarar!")  
+// }
+//  if (age > 50 && age < 60) {
+//     console.log("Sog'liq uchun zarar!")  
+// }
+
+
+//  0 - 20 sovuq
+//  20- 40 issiq
+//  40 - 50  juda issiq
+
+let weather = 555;
+
+// if (weather >= 0 && weather <= 20) {   // 0-20
+//     console.log("Sovuq!")
+// } else if (weather > 20 && weather < 40){ // 20-40
+//     console.log("Issiq") 
+// } else if (weather > 40 && weather < 50) {                  // 40-50
+//     console.log('juda issiq')
+// } else if (weather < 0) {   //-0
+//     console.log("Juda sovuq")
+// } else {
+//     console.log("Juda JUda Juda issiq")
+// }
+
+// !Ternary
+
+// if (weather < 0 ) {
+//     console.log("Sovuq!")
+// } else if (weather > 0 && weather < 10) {
+//     console.log("Iliq")
+// } else {
+//     console.log("Issiq")
+// }
+
+// weather === 20 ? console.log("Sovuq") : console.log("Issiq")
+
+// weather < 0 ? console.log("Sovuq") : weather > 10 ? console.log("Issiq") : console.log('Iliq')
+
+// checkpassword
+
+// const password = "%&^^(*^((654654"
+
+// if (password.toLowerCase() === password.toUpperCase()){
+//     console.log("Kuchli")
+// } else {
+//     console.log("Kuchsiz")
+// }
+
+//! Operatorlar
+
+// =
+// == 
+// ===
+// ! -> NOT
+
+// console.log(2 !== "2")
+
+// && -> AND
+
+// console.log(5 < 10 && 50 > 90)
+// console.log(true && true) //true
+// console.log(true && false) //false
+// console.log(false && true) //false
+// console.log(false && false && false && true && true && true) //false
+
+// console.log("" && "Aipply" && 10) // Aipply
+
+// || -> OR
+
+// console.log(true || false || 0 || "" || false || true) 
+
+// console.log("Azizbek" || "Aipply")
+
+// let a = 10;
+
+// // a++ // a = a + 1
+// a-- // a = a - 1
+
+// console.log(a)
+
+
+
+// let x = 1 , y = 1;
+
+// x++
+// ++y
+
+// console.log(x++)
+// console.log(++y)
+
+// console.log("x=" + x)
+// console.log("y=" + y)
+
+let a = 10;
+a %= 5;
+
+// a = a % 5
+
+console.log(!("kbdjbn" + "dkjvndkjvn" + 50) + true) // 1
