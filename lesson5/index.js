23a9e53fb782ff0ff7a662034b21100b51531a48
jasurbek <jasurdev1604@gmail.com>
2024-09-28 20:06:06 +0500

    for while do while

---
+++ lesson5/index.js
@@ -0,0 +1,52 @@
+for (let i = 0; i < 10; i++) {
+  //   console.log("Hello world");
+}
+
+let i = 0;
+for (; i < 10; ) {
+  //   console.log(i);
+  i++;
+}
+
+// while (candition){
+//     code
+//     inc
+//     dec
+// }
+
+// let a = 50;
+// while (true) {
+//   console.log(a);
+//   a += 2;
+
+//   //   if (a === 100) {
+//   //     break;
+//   //   }
+// }
+
+// let b = 100;
+// do {
+//   console.log(b);
+//   b++;
+// } while (b < 50);
+
+for (let i = 0; i < 5; i++) {
+  for (let j = 1; j < i; j++) {
+    // console.log("j " + j);`
+  }
+  //   console.log("=====================================");
+  //   console.log("i " + i);
+}
+
+/// toq yoki jufligino aniqlash 1 - 10
+
+//  1 toq
+// 2 juft
+
+for (let a = 0; a <= 10; a++) {
+  if (a % 2) {
+    console.log(a + " toq son");
+  } else {
+    console.log(a + " juft son");
+  }
+}
